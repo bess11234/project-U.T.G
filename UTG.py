d59bd60ca735808a81570a680eaf36b462bf4a60
Mew_miruku <109953842+hinatsu13@users.noreply.github.com>
2022-11-15 11:53:16 +0700

    อัพไปก่อน

--- UTG.py
+++ UTG.py
@@ -1,8 +1,16 @@
 import status as st
-def power_player(status_player, stack_player):
+import item as it
+import tutorial as tu
+"""Ultimate tower super ultra Character Galaxy of god (UTG)"""
+def power_player_items(status_player, weapon_status):
+    status_player['str'] += weapon_status["str"]
+    status_player['int'] += weapon_status["int"]
+    status_player['agi'] += weapon_status["agi"]
+
+def power_player(status_player, stack_player, weapon_status):
     status_player['hp'] += stack_player
-    status_player['hp'] += status_player['str']*5
-    status_player['mp'] += status_player['int']*5
+    status_player['hp'] += status_player['str']*5 - weapon_status['str']*5
+    status_player['mp'] += status_player['int']*5 - weapon_status['int']*5
     
 def power_mon(mon, stack):
     mon['str'] += stack
@@ -10,30 +18,41 @@ def power_mon(mon, stack):
     mon['hp'] += mon['str']*5
     mon['mp'] += mon['int']*5
 
-def inside_tower():
+def inside_tower(level, weapon_status, name):
     """tower"""
-    
-    print(st.random_mon,st.Monster[st.random_mon])
     stack_mon, stack_player = 0, 0
-    Tower = 1
-    stack_mon += 1 + Tower//10 #ถ้าจะฟาร์มต่อจะไม่บวกเพิ่ม
-    stack_player += 5
-    status_mon = st.Monster[st.random_mon].copy()
-    status_player = st.Player.copy()
-    power_mon(status_mon, stack_mon)
-    power_player(status_player, stack_player)
 
-"""Ultimate tower super ultra Character Galaxy of god (UTG)"""
+    while level != 51:
+        stack_mon += 1 + level//10 #ถ้าจะฟาร์มต่อจะไม่บวกเพิ่ม
+        stack_player += 5
+        status_mon = st.Monster[st.random_mon].copy()
+        status_player = st.Player.copy()
+        power_player_items(stack_player, weapon_status)
+        power_mon(status_mon, stack_mon)
+        power_player(status_player, stack_player, weapon_status)
+
+        level += 1
+def tower(object, choice, name):
+    """เล่น"""
+    level = 1
+    if object == "ดาบเก่าๆ":
+        object = it.weapon["ดาบ"].copy()
+    if object == "สมุดเวทผุๆ":
+        object = it.weapon["สมุดเวท"].copy()
+    if choice == "2":
+        tu.tutorial(level, object, name)
+    else:
+        inside_tower(level, object, name)
 def main_story():
     """main story"""
-    print("ณ โลกที่แสนสงบสุข ได้มีวิญญาณร้ายกลายร่างเสกหอคอยแห่งความชั่วร้ายขึ้นมาหวังที่จะทำลายร้างโลกทิ้งไป\n\
-        พระราชาจึงได้ประกาศใครที่สามารถทำลายหอคอยนี้ทิ้งไปได้จะบันดาลคำขอให้ 3 อย่าง\n\
-            นับแต่นั้นเป็นต้นมา โลกก็ได้เข้าสู่ยุคสมัยของผู้กล้า ทุกคนต่างรวมตัวกันเพื่อที่จะเคลียร์หอคอย")
+    print("""ณ โลกที่แสนสงบสุข ได้มีวิญญาณร้ายกลายร่่างเสกหอคอยแห่งความชั่วร้ายขึ้นมาหวังที่จะทำลายร้างโลกทิ้งไป
+พระราชาจึงได้ประกาศว่า ใครที่สามารถทำลายหอคอยนี้ทิ้งได้จะบันดาลคำขอให้ 3 อย่าง นับแต่นั้นเป็นต้นมา
+โลกก็ได้เข้าสู่ยุคสมัยของผู้กล้า ทุกคนต่างรวมตัวกันเพื่อที่จะทำลายหอคอย\n""")
     #เลือกอาวุธ
     print("ท่านอยากใช้สิ่งใดเป็นอาวุธ")
     print("1 ดาบเก่าๆ, 2 สมุดเวทผุๆ")
     while True:
-        choice = input()
+        choice = input("กรุณาเลือกอาวุธ : ")
         if choice == "1":
             weapon = "ดาบเก่าๆ"
             break
@@ -43,14 +62,14 @@ def main_story():
         else:
             print("เฮ้ นั้นมันไม่ใช่อาวุธที่นายมีอยู่นะ!!!")
     #บอกชื่อและถามว่าจะข้ามบทฝึกสอนรึเปล่า
-    print("ท่านก็เป็นคนคนนึงที่กำลังจะเข้าสู่หอคอทันใดนั้นก็มีเสียงเข้ามาในหัวท่าน")
+    print("ท่านก็เป็นคนคนหนึ่งที่กำลังจะเข้าสู่หอคอยทันใดนั้นก็มีเสียงเข้ามาในหัว")
     print('เสียงลึกลับ: "ท่านมีนามว่าอะไร?"')
-    name = input()
+    name = input("ชื่อของคุณ : ")
     print("เสียงลึกลับ: \"ว้าวววว!!! ท่าน %s ท่านช่างดูสง่าราศี ข้าคือ [พระเจ้า] ไม่ทราบว่าท่านเป็นผู้กลับชาติมาเกิดใช่รึไม่?\""%name)
-    print("อธืบาย:ผู้กลับชาติมาเกิดหรือก็คือท่านเคยเล่นเกมนี้หรือไม่ตัวเกมจะได้สอนระบบบื้องต้น")
+    print("อธืบาย:ผู้กลับชาติมาเกิดหรือก็คือท่านเคยเล่นเกมนี้หรือไม่ตัวเกมจะได้สอนระบบบื้องต้น\n")
     print("1 ใช่แล้วฉันนี้้แหละผู้กลับชาติมาเกิด!!!, 2 อ่อไม่ใช่อะ")
     while True:
-        choice = input()
+        choice = input("กรุณาเลือกตัวเลือก : ")
         if choice == "1":
             print()
             break
@@ -58,5 +77,6 @@ def main_story():
             print()
             break
         else:
-            "เฮ้ ที่นี้เราไม่ทำกันแบบนั้น!!!"
+            print("เฮ้ ที่นี้เราไม่ทำกันแบบนั้น!!!")
+    tower(weapon, choice, name)
 main_story()
--- item.py
+++ item.py
@@ -1,11 +1,24 @@
 """item"""
 #ผ่าน 5 ชั้น stat อาวุธ จะเพิ่มขึ้น 5 stat
-def item():
-    """item in game Ultimate tower super ultra Character Galaxy of god"""
-    import random
-    rate_drop = ["แย่", "งั้นๆ", "งั้นๆ", "งั้นๆ", "งั้นๆ", "งั้นๆ", "ดีเยี่ยม", "ดีเยี่ยม", "ดีเยี่ยม", "ตำนานจนละเอียด"]
-    weapon = {"ดาบ": {"str": 5, "int": 0, "agi": 3, "skill": ["ฟันตรง", "ยกชิดจ้วงแทง"]}, 
+import random   
+"""item in game Ultimate tower super ultra Character Galaxy of god"""
+rate_drop = ["แย่", "งั้นๆ", "งั้นๆ", "งั้นๆ", "งั้นๆ", "งั้นๆ", "ดีเยี่ยม", "ดีเยี่ยม", "ดีเยี่ยม", "ตำนานจนละเอียด"]
+drop = ["ดาบ", "สมุดเวท", "หอก", "ดาบยักษ์", "ดาบสามมือ"]
+weapon = {"ดาบ": {"str": 5, "int": 0, "agi": 3, "skill": ["ฟันตรง", "ยกชิดจ้วงแทง"]}, 
               "สมุดเวท": {"str": 0, "int": 5, "agi": 3, "skill": ["เวทธาตุ", ""]}, 
               "หอก": {"str": 6, "int": 0, "agi": 4, "skill": ["แทงตรง", "ยกชิดจ้วงแทง"]}, 
               "ดาบยักษ์": {"str": 10, "int": 0, "agi": -1, "skill": ["ฟันตรงแบบแรง!!!", ""]}, 
               "ดาบสามมือ": {"str": 8, "int": 0, "agi": 5, "skill": ["ฟันสามต่อ", ""]}}
+        
+random_rate = random.choice(rate_drop)
+random_drop = random.choice(drop)
+if random_rate == "แย่":
+    add = (-1, -1, -1)
+if random_rate == "ดีเยี่ยม":
+    add = (5, 5, 5)
+if random_rate == "ดีเยี่ยม":
+    add = (*2, *2, *2)
+gain = weapon[random_drop]
+gain["str"] += add[0]
+gain["int"] += add[1]
+gain["agi"] += add[2]
