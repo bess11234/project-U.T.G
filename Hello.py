0879d42f7129b853137bdc2e8008f416a04642af
bess11234 <109953254+bess11234@users.noreply.github.com>
2022-11-16 19:49:36 +0700

    แก้ๆ

--- Hello.py
+++ Hello.py
@@ -1,10 +1,6 @@
 def main():
     from PIL import Image
-    from image import DrawImage
     import climage
-    tmp = "1.png"
-    image = DrawImage.from_file(tmp, size=(40, 20))
-    image.draw_image()
     
     output = climage.convert('1.png', is_unicode=True, width=80)
     print(output)
