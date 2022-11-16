3d44a3a5b9a673fed0b96365c19ee65747908d1e
bess11234 <109953254+bess11234@users.noreply.github.com>
2022-11-16 18:10:06 +0700

    print image

--- Hello.py
+++ Hello.py
@@ -1,7 +1,11 @@
 def main():
     from PIL import Image
     from image import DrawImage
-    tmp = "water_slime.png"
+    import climage
+    tmp = "1.png"
     image = DrawImage.from_file(tmp, size=(40, 20))
     image.draw_image()
+    
+    output = climage.convert('1.png', is_unicode=True, width=80)
+    print(output)
 main()
